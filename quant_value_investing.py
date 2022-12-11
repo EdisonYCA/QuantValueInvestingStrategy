939b8416874f86c56e00db736b508616ad71baf1
EdisonYCA <edisonyca127@gmail.com>
2022-12-10 23:21:06 -0500

    call API on batch stocks and get each stocks data, append all information to dataframe as a new row. Remove all glamour stocks

--- quant_value_investing.py
+++ quant_value_investing.py
@@ -10,15 +10,41 @@ from api_token import IEX_CLOUD_TOKEN
 
 def main():
     stocks = pd.read_csv("sp_500_stocks.csv")  # change to get_file function
+    stocks = stocks[~stocks['Ticker'].isin(['DISCA', 'HFC', 'VIAC', 'WLTW'])]  # DELETE WHEN DONE WITH PROGRAM
 
     # creating dataframe
-    # api_endpoint = f"https://sandbox.iexapis.com/stable/stock/AAL/quote?token={IEX_CLOUD_TOKEN}"
-    # data = requests.get(api_endpoint).json()
     stocks_batch = list(split_list(stocks["Ticker"], 100))
     stocks_batch_strings = []
     for i in range(0, len(stocks_batch)):
-        print(stocks_batch[i])
+        stocks_batch_strings.append(','.join(stocks_batch[i]))
 
+    columns = ["Stock", "Price", "Price to Earnings", "Numbers of Shares to Buy"]
+    dataframe = pd.DataFrame(columns=columns)
+
+    # append each stock as new row of information
+    for stock_str in stocks_batch_strings:
+        # call api at each batch
+        api_endpoint = f"https://sandbox.iexapis.com/stable/stock/market/batch?symbols={stock_str}" \
+                       f"&types=quote&token={IEX_CLOUD_TOKEN}"
+        data = requests.get(api_endpoint).json()
+        # append each row
+        for stock in stock_str.split(','):
+            row = pd.DataFrame([stock,
+                                data[stock]['quote']['latestPrice'],
+                                data[stock]['quote']['peRatio'],
+                                "N/A"],
+                               index=columns).T
+            dataframe = pd.concat((dataframe, row), ignore_index=True)
+
+    # removing glamour stocks
+    dataframe.sort_values("Price to Earnings", inplace=True)
+    # removes all rows that have negative earnings ratio
+    dataframe = dataframe[dataframe["Price to Earnings"] > 0]
+    # removes all rows that are not in top 50
+    dataframe = dataframe[:50]
+    # reset indices of each row
+    dataframe.reset_index(inplace=True, drop=True)
+    print(dataframe)
 
 def get_file():
     """Prompt the user for a csv file containing a list of stocks
